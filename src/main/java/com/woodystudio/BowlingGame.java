e03680fd95abb73012548f9e81289c290d594442
WoodyYan <colorguitar@hotmail.com>
2018-06-29 14:35:10 +0800

    add round score

--- src/main/java/com/woodystudio/BowlingGame.java
+++ src/main/java/com/woodystudio/BowlingGame.java
@@ -4,7 +4,7 @@ public class BowlingGame {
 }
 
 // input 123/451234X, output 300.
-// as a round score, i have three scores, first score, second score, total score. i have next score and previous score.
+// as a round score, i have three scores, first score, second score, total score. i have next score and previous score. 10
 // as a bowling game, i can init 10 empty scores.
 // as a bowling game, i can translate input to 10 empty score.
 // as a bowling game, i can calculate total score.
--- src/main/java/com/woodystudio/RoundScore.java
+++ src/main/java/com/woodystudio/RoundScore.java
@@ -1,13 +1,49 @@
 package com.woodystudio;
 
 public class RoundScore {
-    private enum ScoreType {
-        ONE(1), TWO(2), THREE(3), FOUR(), FIVE, SIX, SEVEN, EIGHT, NINE, SLASH, X;
+    private ScoreType firstScore;
+    private ScoreType secondScore;
+    private int totalScore;
+    private RoundScore previous;
+    private RoundScore next;
 
-        private final int value;
+    public ScoreType getFirstScore() {
+        return firstScore;
+    }
 
-        ScoreType(int value) {
-            this.value = value;
-        }
+    public void setFirstScore(ScoreType firstScore) {
+        this.firstScore = firstScore;
+    }
+
+    public ScoreType getSecondScore() {
+        return secondScore;
+    }
+
+    public void setSecondScore(ScoreType secondScore) {
+        this.secondScore = secondScore;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public RoundScore getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(RoundScore previous) {
+        this.previous = previous;
+    }
+
+    public RoundScore getNext() {
+        return next;
+    }
+
+    public void setNext(RoundScore next) {
+        this.next = next;
     }
 }
